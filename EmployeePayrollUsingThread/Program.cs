1c0da78eefb8ce89d2fcc6f2639f87f20051fdbf
piyush-shaw <piyushshaw0000@gmail.com>
2023-03-01 11:56:05 +0530

    [Piyush] added to solve employee payroll using thread and task parallel

---
+++ EmployeePayrollUsingThread/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace EmployeePayrollUsingThread
+{
+    public  class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Employee Payroll using Threads");
+            string[] words = Demo.CreateWordArray(@"http://www.gutenberg.org/files/54700/54700-0.txt");
+
+            #region ParallelTasks
+            Parallel.Invoke(() =>
+            {
+                Console.WriteLine("Begin first task...");
+                Demo.GetLongestWord(words);
+            },// close 1st action
+            () =>
+            {
+                Console.WriteLine("Begin second task...");
+                Demo.GetMostCommonWords(words);
+            },
+            () =>
+            {
+                Console.WriteLine("Begin third task...");
+                Demo.GetMostCommonWords(words);
+            }// close third action
+            );// close parallel.invoke
+            #endregion
+
+
+        }
+    }
+}
